922414a65310ee95f023a372f8f8d9cb9b3866a0
Alexander Shpilka <alexander.shpilka@kruschecompany.com>
2015-02-22 01:36:41 +0200

    timer page

--- client/views/stepPage/stepSlide.js
+++ client/views/stepPage/stepSlide.js
@@ -1,3 +1,40 @@
-/**
- * Created by philips on 2/22/15.
- */
+Template.stepSlide.events({
+    "click .btn-start-timer": function setTimer() {
+        var timerTime = this.timerTime;
+        Meteor.startup(function () {
+            var now = new Date().getTime(),
+                stepTimer = 5,
+                alarm_time = new Date(now + stepTimer * 1000);
+
+            navigator.plugins.alarm.set(alarm_time,
+                function () {
+                    setTimeout(function () {
+                        Router.go('timerPage');
+                        var my_media = new Media('http://soundbible.com/grab.php?id=2061&type=mp3',
+//                        // success callback
+                            function () {
+                                console.log("playAudio():Audio Success");
+                            },
+                            // error callback
+                            function (err) {
+                                console.log("playAudio():Audio Error: " + err);
+                            }
+                        );
+//                    // Play audio
+                        my_media.play();
+                        document.addEventListener("backbutton", function () {
+                            my_media.stop();
+                            history.go(-1);
+                        })
+                    }, stepTimer * 1000);
+
+                },
+                function () {
+                    // ERROR
+                    console.log('something is wrong');
+
+                })
+        });
+    }
+
+});
