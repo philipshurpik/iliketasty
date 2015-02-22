ab690a77e45fe2f684f63f692adb90d02ed07f60
philipshurpik <philip.shurpik@gmail.com>
2015-02-22 11:54:57 +0200

    timers functionality 2

--- client/views/stepPage/stepSlide.js
+++ client/views/stepPage/stepSlide.js
@@ -15,53 +15,13 @@ Template.stepSlide.events({
             if (Meteor.isCordova) {
                 setAlarm();
             }
+            else {
+                onTimerCallback();
+            }
 
             function setAlarm() {
                 navigator.plugins.alarm.set(alarm_time,
-                    function () {
-                        $('.tab-timer').css('display', 'table-cell');
-                        $('.tab-timer').find('p').html(stepTimer);
-
-                        var interval = window.setInterval(function () {
-                            stepTimer--;
-                            $('.tab-timer').find('p').html(stepTimer);
-                            if (stepTimer === 0) {
-                                window.clearInterval(interval);
-                            }
-                        }, 1000);
-
-                        setTimeout(function () {
-                            window.my_media = new Media('http://soundbible.com/grab.php?id=2061&type=mp3',
-//                        // success callback
-                                function () {
-                                    console.log("playAudio():Audio Success");
-                                },
-                                // error callback
-                                function (err) {
-                                    console.log("playAudio():Audio Error: " + err);
-                                }
-                            );
-//                    // Play audio
-                            window.my_media.play();
-                            popupEl.css('display', 'block');
-
-                            popupEl.find('.btn-end-timer').on('click', function () {
-                                popupEl.css('display', 'none');
-                                window.my_media.stop();
-                                var cur_scroll = parseInt($('.slide-group')[0].style.webkitTransform.match(/translate3d\(([^,]*)/)[1], 10),
-                                    new_scroll = -(Math.abs(cur_scroll) + document.body.offsetWidth);
-                                $('.slide-group')[0].style.webkitTransform = 'translate3d(' + new_scroll + 'px,0,0)';
-
-                                $('.tab-timer').css('display', 'none');
-                            });
-
-                            document.addEventListener("backbutton", function () {
-                                window.my_media.stop();
-                                history.go(-1);
-                            })
-                        }, stepTimer * 1000);
-
-                    },
+                    onTimerCallback,
                     function () {
                         // ERROR
                         console.log('something is wrong');
@@ -70,5 +30,52 @@ Template.stepSlide.events({
             }
 
         });
+
+        function onTimerCallback() {
+            var activeTimer = timerTime;
+            Session.set('activeTimer', activeTimer);
+            var interval = window.setInterval(function () {
+                activeTimer-=1;
+                Session.set('activeTimer', activeTimer > 0 ? activeTimer : "");
+                if (activeTimer === 0) {
+                    window.clearInterval(interval);
+                }
+            }, 1000);
+
+            setTimeout(function () {
+                if (Media) {
+                    window.my_media = new Media('http://soundbible.com/grab.php?id=2061&type=mp3',
+//                        // success callback
+                        function () {
+                            console.log("playAudio():Audio Success");
+                        },
+                        // error callback
+                        function (err) {
+                            console.log("playAudio():Audio Error: " + err);
+                        }
+                    );
+//                    // Play audio
+                    window.my_media.play();
+                }
+
+                popupEl.css('display', 'block');
+
+                popupEl.find('.btn-end-timer').on('click', function () {
+                    popupEl.css('display', 'none');
+                    window.my_media.stop();
+                    var cur_scroll = parseInt($('.slide-group')[0].style.webkitTransform.match(/translate3d\(([^,]*)/)[1], 10),
+                        new_scroll = -(Math.abs(cur_scroll) + document.body.offsetWidth);
+                    $('.slide-group')[0].style.webkitTransform = 'translate3d(' + new_scroll + 'px,0,0)';
+
+                    $('.tab-timer').css('display', 'none');
+                });
+
+                document.addEventListener("backbutton", function () {
+                    window.my_media.stop();
+                    history.go(-1);
+                })
+            }, timerTime * 1000);
+
+        }
     }
 });
